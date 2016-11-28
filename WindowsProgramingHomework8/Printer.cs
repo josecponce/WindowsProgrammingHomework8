56e1247050c34c586feae2f2b389d1ec7a51e1e2
Daylin Gonzalez <dgonz324@fiu.edu>
2016-11-28 03:34:08 -0500

    Print one text element per row. Include all the properties per row. Arrange the data tabularly

--- WindowsProgramingHomework8/Printer.cs
+++ WindowsProgramingHomework8/Printer.cs
@@ -15,6 +15,7 @@ namespace Homework10 {
         private PrintDocument pd;
         Form parent;
         int pageNumber;
+        int lastIndex;
 
         public Printer(TextsDocument document, Form parent) {
             this.document = document;
@@ -26,6 +27,7 @@ namespace Homework10 {
             printDialog.Document = pd;
             printDialog.UseEXDialog = true;
             pageNumber = 1;
+            lastIndex = 0;
 
 
             // Add a PrintPageEventHandler for the document 
@@ -38,85 +40,94 @@ namespace Homework10 {
         {          
             Graphics g = ev.Graphics;
             Rectangle rec = ev.MarginBounds;
+            var font = new Font("Times New Roman", 8);
 
 
-            g.DrawString("Group 2", new Font("Times New Roman", 8), new SolidBrush(Color.Black), new Point(100, 50));
+            g.DrawString("Group 2", font, new SolidBrush(Color.Black), new Point(100, 50));
             string h = "Homework Assignment 10";
             var m = g.MeasureString(h, new Font("Times New Roman", 8));
-            g.DrawString(h, new Font("Times New Roman", 8), new SolidBrush(Color.Black), new Point(rec.Width + 100 - (int)m.Width, 50));
+            g.DrawString(h,font , new SolidBrush(Color.Black), new Point(rec.Width + 100 - (int)m.Width, 50));
             g.DrawLine(new Pen(Color.Black), new Point(100, 80), new Point(rec.Width + 100, 80));
             g.DrawLine(new Pen(Color.Black), new Point(100, rec.Height + 120), new Point(rec.Width + 100, rec.Height + 120));
             DateTime date = DateTime.Today;
-            g.DrawString(date.ToShortDateString(), new Font("Times New Roman", 8), new SolidBrush(Color.Black), new Point(100, rec.Height + 150));
-            g.DrawString(pageNumber.ToString(), new Font("Times New Roman", 8), new SolidBrush(Color.Black), new Point(rec.Width/2 + 100, rec.Height + 150));
+            g.DrawString(date.ToShortDateString(), font, new SolidBrush(Color.Black), new Point(100, rec.Height + 150));
+            g.DrawString(pageNumber.ToString(), font, new SolidBrush(Color.Black), new Point(rec.Width/2 + 100, rec.Height + 150));
 
+            int col = 0;
+            int row = 0;
+            font = new Font("Times New Roman", 14);
+            g.DrawString("Text", font, new SolidBrush(Color.Black), new Point((rec.Width / 5) * col++ + 100, 110));
+            g.DrawString("Font", font, new SolidBrush(Color.Black), new Point((rec.Width / 5) * col++ + 100, 110));
+            g.DrawString("Location", font, new SolidBrush(Color.Black), new Point((rec.Width / 5) * col++ + 100, 110));
+            g.DrawString("Rotation", font, new SolidBrush(Color.Black), new Point((rec.Width / 5) * col++ + 100, 110));
+            g.DrawString("ZOrder", font, new SolidBrush(Color.Black), new Point((rec.Width / 5) * col++ + 100, 110));
+            row++;
 
+            var mes = g.MeasureString("Text", font);
 
             int numberOfItems = document.Texts.Count;
 
-            float x = 0;
-            float y = 2;
-            PointF def = new PointF(0, 0);
+            float x = 100;
+            float y = 100 + mes.Height + 5;            
 
             List<Text> zorderedTexts = new List<Text>(document.Texts);
-            zorderedTexts.Sort((one, two) => (one.ZOrder < two.ZOrder) ? -1 : 1);
-            for (int i = 0; i < numberOfItems; i++)
+            font = new Font("Times New Roman", 8);
+
+            for (int i = lastIndex; i < numberOfItems; i++)
             {
-                x = zorderedTexts[i].Location.X;
-                y = zorderedTexts[i].Location.Y + 100;
-                
-                zorderedTexts[i].Location = new PointF(x, y);
-                //size:
-                float size = zorderedTexts[i].Font.Size;
-
-                //Font:
-                var font = zorderedTexts[i].Font;
-
                 //text that will be displayed:
-                string toDraw = "test draw";
-                toDraw = zorderedTexts[i].TextToDraw;
+                string toDraw = zorderedTexts[i].TextToDraw;
+                if (toDraw != "")
+                {
+                    col = 0;
 
-                //color:
-                Brush brush = new SolidBrush(Color.Black);
-
-                float rotation = (float)zorderedTexts[i].Rotation;
-                SizeF textSize = g.MeasureString(zorderedTexts[i].TextToDraw, font);
-
-                //move rotation point to center of image
-                g.TranslateTransform(textSize.Width / 2 + x, textSize.Height / 2 + y);
-                //rotate
-                g.RotateTransform(rotation);
-
-               
-                g.DrawString(toDraw, font, brush, -(textSize.Width / 2), -(textSize.Height / 2));
-                g.ResetTransform();
-
-            }//end forloop
-             //try {
+                    //location:
+                    PointF loc = zorderedTexts[i].Location;
+                    //Font:
+                    var fontName = zorderedTexts[i].Font.Name;
+                    //rotation
+                    float rotation = (float)zorderedTexts[i].Rotation;
+                    //zOrder
+                    int zorder = zorderedTexts[i].ZOrder;
 
 
-            //    foreach (Text t in document.Texts)
-            //    {
-            //        // Create and initialize print font 
-            //        using (Font printFont = t.Font)
-            //        {
-            //            // Draw a line of text, followed by the chart 
-            //            ev.Graphics.DrawString(t.TextToDraw, printFont, Brushes.Black, 10, 10);
-            //        }
-            //    }
+                    //color:
+                    Brush brush = new SolidBrush(Color.Black);
 
-            //    // Create and initialize print font 
-            //    using (Font printFont = new Font("Arial", 10)) {
-            //        // Draw a line of text, followed by the chart 
-            //        ev.Graphics.DrawString("Line before chart", printFont, Brushes.Black, 10, 10);
-            //    }
-            //    // Create Rectangle structure, used to set the position of the chart Rectangle 
-            //    //var myRec = new Rectangle(100, 100, chart.Bounds.Right, chart.Bounds.Bottom);
-            //    //chart.Printing.PrintPaint(ev.Graphics, myRec);
-            //} catch (Exception e) {
-            //        Console.WriteLine(e.Message);
-            //    }
-            pageNumber++;
+                    mes = g.MeasureString(toDraw, font);
+
+                    x = (rec.Width / 5) * col++ + 100;
+                    g.DrawString(toDraw, font, brush, x, y);
+                    x = (rec.Width / 5) * col++ + 100;
+                    g.DrawString(fontName, font, brush, x, y);
+                    x = (rec.Width / 5) * col++ + 100;
+                    g.DrawString(loc.ToString(), font, brush, x, y);
+                    x = (rec.Width / 5) * col++ + 100;
+                    g.DrawString(rotation.ToString(), font, brush, x, y);
+                    x = (rec.Width / 5) * col++ + 100;
+                    g.DrawString(zorder.ToString(), font, brush, x, y);
+
+
+                    y = y + mes.Height + 5;
+                    if (y >= rec.Height + 100)
+                    {
+                        ev.HasMorePages = true;
+                        y = 0;
+                        lastIndex = i;
+                        pageNumber++;
+                        return; // you need to return, then it will go into this function again
+                    }
+                    else
+                    {
+                        ev.HasMorePages = false;
+                    }
+                        
+
+                    
+                }
+                
+            }
+            
         }
 
         public void ShowDialog() {
